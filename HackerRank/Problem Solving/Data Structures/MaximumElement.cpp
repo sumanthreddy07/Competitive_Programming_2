fcf4d5de9b5256a66162523f4b215ecf8d802428
sumanthreddy07 <kota.sumanthreddy@gmail.com>
2025-06-20 23:39:52 +0530

    Stacks

---
+++ HackerRank/Problem Solving/Data Structures/MaximumElement.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'getMax' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts STRING_ARRAY operations as parameter.
+ */
+
+vector<int> getMax(vector<string> operations) {
+    stack<int> res, tracker;
+    vector<int> ret;
+    for(int i=0;i<operations.size();i++)
+    {
+        
+        if(operations[i][0]=='1')
+        {
+            string val = operations[i].substr(2);
+            int vals = stoi(val);
+            if(res.empty())
+                res.push(vals);
+            else
+                res.push(max(res.top(), vals));            
+        }
+        else if(operations[i][0]=='2')
+            res.pop();
+        else {
+              ret.push_back(res.top());                  
+        }
+    }
+    
+    return ret;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> ops(n);
+
+    for (int i = 0; i < n; i++) {
+        string ops_item;
+        getline(cin, ops_item);
+
+        ops[i] = ops_item;
+    }
+
+    vector<int> res = getMax(ops);
+
+    for (size_t i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
